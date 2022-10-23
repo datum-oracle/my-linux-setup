bf673d1f5e9d01d7be4fe3ff9c27fa627ff42396
Dminor7 <contact.dshukla@gmail.com>
2022-10-23 14:27:00 +0530

    Setting up python

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# my-linux-setup
-# my-linux-setup
+my-linux-setup
+---
+
+[Setup shell env for pyenv](https://github.com/pyenv/pyenv#set-up-your-shell-environment-for-pyenv)
---
+++ install-python.sh
@@ -0,0 +1,6 @@
+sudo apt update; sudo apt install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.8 python3-pip
